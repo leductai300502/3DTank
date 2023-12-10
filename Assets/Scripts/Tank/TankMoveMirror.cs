06051a6e0de2704c74ff4edfe923eb855c70b8d1
leductai300502 <tai.leductai@hcmut.edu.vn>
2023-12-10 21:42:56 +0700

    Networking Mirror

---
+++ Assets/Scripts/Tank/TankMoveMirror.cs
@@ -0,0 +1,15 @@
+﻿using Mirror;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TankMoveMirror : NetworkBehaviour
+{
+    // Start is called before the first frame update
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
